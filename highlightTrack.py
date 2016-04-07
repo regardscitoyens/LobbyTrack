cf11f55a86afe58df673585437b67ec4ee0f8374
Tangui Morlier <tangui@tangui.eu.org>
2016-04-07 20:34:35 +0200

    Correction d'un bug de version python3 chez PLTV

--- highlightTrack.py
+++ highlightTrack.py
@@ -7,7 +7,7 @@ ngram = 4
 
 def file2array(file):
     mots = []
-    for mot in re.split("\s+", re.sub('L(\d)', r'L \1', re.sub('([«"»])', r' \1 ', re.sub('\n+', ' <br/> ', re.sub(u'’', "'", re.sub("[\?\+‑\.!,;–()'’-]", " ", file.read())))))):
+    for mot in re.split("\s+", re.sub('L(\d)', r'L \1', re.sub('([«"»])', r' \1 ', re.sub('\n+', ' <br/> ', re.sub('’', '\'', re.sub("[\?\+‑\.!,;–()'’-]", " ", file.read())))))):
         if mot:
             mots.append(mot)
     return mots
