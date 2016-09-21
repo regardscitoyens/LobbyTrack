b4614f821f165ea7bca3d8389f2dd50bf1a9e48d
jt <chanegue@gmail.com>
2016-09-21 15:06:02 +0200

    no message

--- lobbyTrack.py
+++ lobbyTrack.py
@@ -3,7 +3,8 @@
 import requests
 import nltk
 from nltk import ngrams
-
+import re
+from collections import Counter
 
 # Variables
 site = "nosdeputes.fr"
@@ -18,7 +19,7 @@ raw = ''.join(raw.splitlines())
 
 # Constuire des N-grams
 # http://stackoverflow.com/questions/17531684/n-grams-in-python-four-five-six-grams
-
+counter = Counter()
 xgrams = ngrams(raw.split(), x)
 for gram in xgrams:
     str_gram = ' '.join(gram)
@@ -27,5 +28,16 @@ for gram in xgrams:
     url = 'http://'+site+'/recherche/'+str_gram+'?format=csv'
     print('-- Url appelé : '+url)
     response = requests.get(url)
-    print ('-- Code retour : ' +response.status_code)
-    response.content
+    print ('-- Code retour : ' +str(response.status_code))
+
+    # Améliorer le résultat du contenu
+    urls = re.findall(b'http://.*/csv', response.content)
+    #print('-- urls :'+urls)
+    for url in urls:
+        url = url.decode("utf-8")
+        counter[url] += 1
+    list = Counter(counter).most_common(3)
+    print(list)
+
+print('------')
+print(Counter(counter).most_common(10))
